1e18db38569cc072870ff523e1c2b2f5ee3ff2e2
Daniel Chikimoni <dnzg666@gmail.com>
2017-05-16 16:39:08 +0200

    install readline-sync, make a function in index.js and export it to the bin

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
+import readlineSyncAskName from '../index';
+
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+
+readlineSyncAskName();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export const readlineSyncAskName = () => {
+  var readlineSync = require('readline-sync');
+  // Wait for user's response.
+  var userName = readlineSync.question('May I have your name? ');
+  console.log('Hi ' + userName + '!');
+}
