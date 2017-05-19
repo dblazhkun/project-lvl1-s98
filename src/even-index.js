92b4f21bec48276c744b69b284da2f648a4385eb
Daniel Chikimoni <dnzg666@gmail.com>
2017-05-19 15:58:44 +0200

    0

--- src/even-index.js
+++ src/even-index.js
@@ -1,11 +1,13 @@
 import readlineSync from 'readline-sync';
 
 const randNum = () => Math.floor(Math.random() * 100);
-
-const evenNumberBrainGame = () => {
+const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
+};
 
+const evenNumberBrainGame = () => {
+  const userName = askName();
   const askEven = () => {
     const getRandNum = randNum();
     const checkAnswer = (answer) => {
