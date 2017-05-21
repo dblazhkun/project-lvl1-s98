626aee3ff898af0d05aee8a5bd3234e8a052c834
Daniel Chikimoni <dnzg666@gmail.com>
2017-05-21 23:02:13 +0200

    complete balance-game

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# project-lvl1-s98
+# Brain Games
 
 [![Code Climate](https://codeclimate.com/github/dnzg666/project-lvl1-s98/badges/gpa.svg)](https://codeclimate.com/github/dnzg666/project-lvl1-s98)
 [![Build Status](https://travis-ci.org/dnzg666/project-lvl1-s98.svg?branch=master)](https://travis-ci.org/dnzg666/project-lvl1-s98)
+
+## installation:
+``` npm i -g danielsbraingames ```
+
+### Start:
+``` brain-even ```
+
+``` brain-calc ```
+
+``` brain-gcd ```
+
+``` brain-balance ```
+
+``` brain-progression ```
+
+``` brain-prime ```
--- src/games/game-balance.js
+++ src/games/game-balance.js
@@ -4,30 +4,38 @@ import { getRandomInRange, engine } from '..';
 const balance = () => {
   const rule = 'Balance the given number.';
   const gameBalance = () => {
-    const getRightAnswer = (num) => {
-      const getArrFromNum = Array.from(num.toString()).map(Number);
-      if (getArrFromNum.every(item => item === getArrFromNum[0])) {
-        return getArrFromNum.join('');
+    const question = getRandomInRange(100, 9999);
+
+    const getBalancedNum = (arr) => {
+      const max = Math.max(...arr);
+      const min = Math.min(...arr);
+      const delta = max - min;
+
+      if (delta <= 1) {
+        return arr.sort((a, b) => a - b).join('');
       }
-      const fixLengthArr = getArrFromNum.slice().fill(0);
-      let sumOfDigits = getArrFromNum.reduce((acc, item) => acc + item);
-      const fn = (arr = fixLengthArr) => {
-        const newArr = arr.reduce((acc, _, i) => {
-          if (sumOfDigits === 0) {
-            return acc;
-          }
-          sumOfDigits -= 1;
-          acc[i] += 1;
-          return acc;
-        }, arr);
-        return sumOfDigits === 0 ? newArr.reverse().join('') : fn(newArr);
-      };
-      return fn();
+
+      const maxIndex = arr.indexOf(max);
+      const minIndex = arr.indexOf(min);
+
+      const updatedArr = arr.slice(0);
+      const shift = Math.floor(delta / 2);
+
+      updatedArr[minIndex] += shift;
+      updatedArr[maxIndex] -= shift;
+
+      return getBalancedNum(updatedArr);
     };
-    const question = getRandomInRange(100, 999);
+
+    const getRightAnswer = (number) => {
+      const numbersArray = String(number).split('').map(x => +x);
+      return getBalancedNum(numbersArray);
+    };
     const rightAnswer = getRightAnswer(question);
+
     return cons(question, rightAnswer);
   };
+
   engine(rule, gameBalance);
 };
 
